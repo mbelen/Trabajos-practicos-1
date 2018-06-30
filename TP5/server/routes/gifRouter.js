9c8936d0f8582619d1d5658caeae9e9a26261f23
elifer aponte <eliferaponte_11@hotmail.com>
2018-06-29 16:46:04 -1200

    incompleto

--- TP5/server/routes/gifRouter.js
+++ TP5/server/routes/gifRouter.js
@@ -28,6 +28,28 @@ gifs.post('/search', async (req, res, next) => {
         usarname: gif.username,
         import_datetime: gif.import_datetime
       }
+
+      $('#container').prepend(`
+        <div class="row">
+                <a href="${BASE_URL}">
+                      <div class="filtro">
+                        ${gif.title}
+                        ${gif.username}                        
+                      </div>
+                      <div class="gif">
+                        ${gif.images}
+                      </div>
+                      <div class="boton">
+                        ${gif.images}
+                        ${gif.title}
+                        ${gif.source}
+                        ${gif.username}
+                      </div>
+                </a>
+        </div>
+      `);
+    });  
+
       return payloads.push(payload)
     });
     
